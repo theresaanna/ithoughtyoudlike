3b0900d7c73843abfaf39ce33bc9a768c844b797
theresaanna <theresa@theresaanna.com>
2012-12-16 12:56:11 -0500

    beginning form submission logic

--- ithoughtyoudlike.js
+++ ithoughtyoudlike.js
@@ -1,39 +1,44 @@
-Urls = new Meteor.Collection('urls');
+Users = new Meteor.Collection('users');
 
 if (Meteor.isClient) {
-  Accounts.ui.config({
-    requestPermissions: {
-      facebook: ['email']
-    }
-  });
-
-  Template.linkForm.events({
-    // submit a new link
-    'click #go' : function() {
-        var url, email;
-        url = $('#url').attr('value');
-        email = $('#email').attr('value');
-
-        Urls.insert({url: url, email: email});
-
-        if (Meteor.user()) {
-          Feedback.pop('Thanks!');
+    Accounts.ui.config({
+        requestPermissions: {
+            facebook: ['email']
         }
-        else {
-          Feedback.pop('Please login to send a link');
+    });
+
+    Template.linkForm.events({
+        // submit a new link
+        'click #go' : function() {
+                var url, email, desc, sender;
+                url = $('#url').attr('value');
+                email = $('#email').attr('value');
+                desc = $('#desc').attr('value');
+                sender = Meteor.user();
+
+                if (sender) {
+                    Feedback.pop('Thanks!', 'main');
+                    Users.insert({url: url, email: email, desc: desc, sender: sender});
+                    // lookup target user
+                    // ask if we should send a notification
+                }
+                else {
+                    Feedback.pop('Please login to send a link', 'userMenu');
+                }
         }
-    }
-  });
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+    Meteor.startup(function () {
 
-  });
+    });
 }
 
-Feedback = {
-  pop: function() {
-    
-  }
+Feedback = {         
+    pop: function(msg, loc) {
+        var domObj = $('#' + loc);
+        domObj.text(msg); 
+        domObj.fadeIn().delay(4000).fadeOut(); 
+    }
 };
