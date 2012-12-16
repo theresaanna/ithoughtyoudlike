6c38c00c4ad2742335936ff0c43d9ce0381432aa
theresaanna <theresa@theresaanna.com>
2012-12-16 13:04:38 -0500

    commenting

--- ithoughtyoudlike.js
+++ ithoughtyoudlike.js
@@ -1,4 +1,18 @@
 /* db collections */
+
+/*
+ * linkRecipient
+ *      name
+ *      email
+ *          links {
+ *              description
+ *              url
+ *              sender
+ *          }
+ *
+ *
+ */
+
 LinkRecipients = new Meteor.Collection('linkRecipients');
 
 /* client events and config */
@@ -13,17 +27,20 @@ if (Meteor.isClient) {
         // submit a new link
         'click #go' : function() {
                 var url, email, desc, sender;
+                // retrieve form data
                 url = $('#url').attr('value');
                 email = $('#email').attr('value');
                 desc = $('#desc').attr('value');
                 sender = Meteor.user();
 
+                // if the link sender is logged in
                 if (sender) {
                     Feedback.pop('Thanks!', 'main');
                     LinkRecipients.insert({url: url, email: email, desc: desc, sender: sender});
                     // lookup target user
                     // ask if we should send a notification
                 }
+                // prompt to login
                 else {
                     Feedback.pop('Please login to send a link', 'userMenu');
                 }
